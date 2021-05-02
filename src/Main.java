41250c746218a397d25bfc5ae982beefefbbf937
Atbravo <at.bravo@uniandes.edu.co>
2021-05-02 15:34:12 -0500

    completo

--- src/Main.java
+++ src/Main.java
@@ -1,97 +1,69 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.util.Date;
 
 public class Main
 {
-	private String algoritmo;
-	String cadena;
+	private String inicial;
+	private int algoritmo;
 	private int numCeros;
+	public final static char[] busqueda =  "abcdefghijklmnopqrstuvwxyz".toCharArray();
 
-	Main(String algoritmo, String cadena, int numCeros)
+	public void leerDatos()
 	{
-		this.algoritmo = algoritmo;
-		this.cadena = cadena;
-		this.numCeros = numCeros;
-	}
-
-	public byte[] calcularHash256(String input) throws NoSuchAlgorithmException
-	{
-		MessageDigest md = MessageDigest.getInstance("SHA-256");
-		return md.digest(cadena.getBytes(StandardCharsets.UTF_8));
-	}
-
-	public byte[] calcularHash512(String input) throws NoSuchAlgorithmException
-	{
-
-		MessageDigest md = MessageDigest.getInstance("SHA-512");
-		return md.digest(cadena.getBytes(StandardCharsets.UTF_8));
-	}
-
-	//Ejemplo slack, borrar despues
-	public void visualizarEjemplo() throws NoSuchAlgorithmException
-	{
-		String cadena = "cadena inicial abc";
-		String v = "aalgsrj";
-		System.out.println("--------EJEMPLO---------");
-		System.out.println("Cadena: " + cadena);
-		System.out.println("V: " + v);
-		MessageDigest md = MessageDigest.getInstance("SHA-512");
-		byte[] hash = md.digest((cadena + v).getBytes(StandardCharsets.UTF_8));
-	 
-		String resultado = pasarAHexa(hash, 512);
-	  
-		System.out.println(resultado);
-	}
-
-	public static void main(String args[])
-	{
+		Minador.seguir = true;
 		try
 		{
 			Scanner sc = new Scanner(System.in);
 			System.out.println("Ingrese algoritmo a utilizar (SHA-256 ó SHA-512)");
-			String algoritmo = sc.nextLine();
+			String alg = sc.nextLine();
+			if (alg.equals("SHA-256"))
+			{
+				algoritmo = 256;
+			}
+			else if (alg.equals("SHA-512"))
+			{
+				algoritmo = 512;
+			}
+			else
+			{
+				throw new Exception();
+			}
 			System.out.println("Ingrese cadena a procesar");
-			String cadena = sc.nextLine();
+			inicial = sc.nextLine();
 			System.out.println("Ingrese el número de ceros esperados");
-			int numCeros = sc.nextInt();
-			Main main = new Main(algoritmo, cadena, numCeros);
-			
-			
-			
-			main.visualizarEjemplo();
+			numCeros = sc.nextInt();
 		}
 		catch (Exception e)
 		{
-			System.out.println("ERROR: Favor revise el formato de la información ingresada");
+			System.out.println("Error en el formato de entrada, Favor reingrese los datos");
+			System.out.println();
+			leerDatos();
+		}
+	}
+ public static void main(String args[])
+	{
+		try
+		{
+			Main main = new Main();
+			while (true)
+			{
+				Date date = new Date();
+				main.leerDatos();
+				long inicio = System.nanoTime();
+				for (int i = 0; i < 26; i++)
+				{
+					Minador minador = new Minador(main.inicial, main.algoritmo, main.numCeros, busqueda[i]+"");
+					minador.run();
+				}
+				long tiempo = System.nanoTime() - inicio;
+				System.out.println("Tiempo de busqueda:" + tiempo/1000000000 + " segundos");
+				System.out.println();
+			}
+			// main.visualizarEjemplo();
+		}
+		catch (Exception e)
+		{
 			e.printStackTrace();
 		}
-		
-	}
-	
-	public String pasarAHexa(byte[] hash, int algoritmo)
-	{
-		int len = 0;
-		if(algoritmo == 256)
-			len = 64;
-		else if(algoritmo == 512)
-			len = 128;
-		BigInteger number = new BigInteger(1, hash); 
-		  
-        // Convert message digest into hex value 
-        StringBuilder hexString = new StringBuilder(number.toString(16)); 
-  
-        // Pad with leading zeros
-        while (hexString.length() < len) 
-        { 
-            hexString.insert(0, '0'); 
-        }
-        return hexString.toString();
 	}
 }
---
+++ src/Minador.java
@@ -0,0 +1,114 @@
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Minador extends Thread
+{
+	public static boolean seguir;
+
+	private String inicial;
+	private int algoritmo;
+	private int numCeros;
+	private String asignacion;
+	private boolean seguirInterno;
+
+	public Minador(String inicial, int alg, int numCeros, String asignacion)
+	{
+		this.inicial = inicial;
+		algoritmo = alg;
+		this.numCeros = numCeros;
+		this.asignacion = asignacion;
+		seguirInterno = true;
+	}
+
+	public byte[] calcularHash(String input) throws NoSuchAlgorithmException
+	{
+		MessageDigest md;
+		if (algoritmo == 512)
+			md = MessageDigest.getInstance("SHA-512");
+		else
+			md = MessageDigest.getInstance("SHA-256");
+		return md.digest(input.getBytes(StandardCharsets.UTF_8));
+	}
+
+	public int darCeros(byte[] hash, int algoritmo)
+	{
+		int len = 0;
+		if (algoritmo == 256)
+			len = 64;
+		else if (algoritmo == 512)
+			len = 128;
+		BigInteger number = new BigInteger(1, hash);
+
+		// Convert message digest into hex value
+		StringBuilder hexString = new StringBuilder(number.toString(16));
+
+		return (len - hexString.length()) * 4;
+	}
+
+	public void asignarMineria(String str) throws Exception
+	{
+		for (int i = 6; i >= 0; i--)
+		{
+			minar(str, 0, i);
+		}
+	}
+
+	public void minar(String v, int pos, int length) throws Exception
+	{
+		if (seguirInterno)
+		{
+			if (length == 0)
+			{
+				String cadena = inicial + v;
+				byte[] hash = calcularHash(cadena);
+				int ceros = darCeros(hash, algoritmo);
+				if (ceros == numCeros)
+				{
+					synchronized (Main.class)
+					{
+						seguir = false;
+					}
+					System.out.println(ceros);
+					System.out.println("Cadena encontrada:");
+					System.out.println("v = " + v);
+					System.out.println("Cadena + v = " + cadena);
+					seguirInterno = false;
+				}
+			}
+			else
+			{
+				if (pos != 0)
+				{
+					pos = 0;
+				}
+				for (int i = 0; i < Main.busqueda.length && seguirInterno; i++)
+				{
+					synchronized (Main.class)
+					{
+						if (!seguir)
+						{
+							seguirInterno = false;
+						}
+					}
+					minar(v + Main.busqueda[i], i, length - 1);
+				}
+			}
+
+		}
+	}
+
+	@Override
+	public void run()
+	{
+		try
+		{
+			asignarMineria(asignacion);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
